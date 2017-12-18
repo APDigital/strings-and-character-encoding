2213e541da7bddd5601791cc13ea0bc963093531
APDigital <aoifejo.pearson@googlemail.com>
2017-12-18 15:48:46 +0000

    csvdata to console table

---
+++ StringsAndCharacterEncodings/StringsAndCharacterEncodings/DataRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndCharacterEncodings
+{
+    class DataRecord
+    {
+        public DateTime PublicationDate { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+    }
+}
--- StringsAndCharacterEncodings/StringsAndCharacterEncodings/Program.cs
+++ StringsAndCharacterEncodings/StringsAndCharacterEncodings/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace StringsAndCharacterEncodings
 {
@@ -10,6 +11,41 @@ namespace StringsAndCharacterEncodings
     {
         static void Main(string[] args)
         {
+            string Title = string.Format("| Pub Date   |                     Title | Authors                   |\n|====================================================================|");
+            Console.WriteLine(Title);
+
+            using (var sr = new StreamReader("../csvdata.csv"))
+            {
+                var reader = new CsvReader(sr);
+                IEnumerable<DataRecord> records = reader.GetRecords<DataRecord>();
+                foreach (var record in records)
+                {
+                    string pubDate = record.PublicationDate.ToString("dd/MM/yyyy");
+                    string title = record.Title;
+                    string author = record.Authors;
+
+                    if (title.Length < 25 && author.Length < 25)
+                    {
+                        Console.WriteLine(string.Format("| {0,-10} | {1,-25} | {2,5} |", pubDate, title, author));
+                    }
+                    else if (title.Length < 25 && author.Length >= 25)
+                    {
+                        Console.WriteLine(string.Format("| {0,-10} | {1,-25} | {2,5} |", pubDate, title, author.Substring(0, 25)));
+                    }
+                    else if (title.Length >= 25 && author.Length < 25)
+                    {
+                        Console.WriteLine(string.Format("| {0,-10} | {1,-25} | {2,-25} |", pubDate, title.Substring(0, 25), author));
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("| {0,-10} | {1,-25} | {2,5} |", pubDate, title.Substring(0, 25), author.Substring(0, 25)));
+                    }
+                   
+
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
+
